33e01f83f767ee01eaaf9be385c8045eb21010a0
Joaquingse <joaquingse@gmail.com>
2022-12-07 13:36:22 +0000

    create restaurant model create restaurant controller create restaurant router

---
+++ api/models/restaurants.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const restaurantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    unique: [true, 'This address is registered']
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  web: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: [true, 'This phone is registered']
+  },
+  burguers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "burger",
+    },
+  ],
+});
+
+
+const restaurantModel = mongoose.model('restaurant', restaurantSchema);
+module.export = restaurantModel
