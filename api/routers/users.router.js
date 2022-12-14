978595593b5bd0af679656b98485dd05cfeb3f38
Tommyscripts <dark.angel.alhue@gmail.com>
2022-12-14 16:08:31 +0000

    Favorite: -Now you can add favorite

--- api/routers/users.router.js
+++ api/routers/users.router.js
@@ -1,16 +1,21 @@
 const router = require("express").Router();
-const { authUser,adminCheck } = require("../utils"); // Authenticated Route
+const { authUser, adminCheck } = require("../utils"); // Authenticated Route
 
 const {
   getUserById,
   deleteUserById,
   updateUser,
-  createUser
+  createUser,
+  getFavoriteBurguer,
+  createFavoriteBurger,
+  updateFavoriteBurger
 } = require("../controllers/users.controllers");
 
 router.get("/profile", authUser, getUserById);
-router.post('/admin',authUser,adminCheck,createUser);
-router.put("/profile",authUser,adminCheck, updateUser);
+router.post("/admin", authUser, adminCheck, createUser);
+router.put("/profile", authUser, adminCheck, updateUser);
 router.delete("/profile", authUser, deleteUserById);
-
+router.get("/favorite", authUser, getFavoriteBurguer);
+router.patch("/favorite/add", authUser, createFavoriteBurger);
+router.patch("/favorite/remove", authUser, updateFavoriteBurger);
 module.exports = router;
